d3644c61b0e531b985fda2ac21fe292e14754018
Aphelios <lrh1886113@outlook.com>
2025-03-30 15:48:27 +0800

    修复后端追踪问题

---
+++ TomatoMall/src/main/java/com/example/tomatomall/controller/AccountController.java
@@ -0,0 +1,68 @@
+package com.example.tomatomall.controller;
+
+import com.example.tomatomall.po.Account;
+import com.example.tomatomall.service.AccountService;
+import com.example.tomatomall.util.TokenUtil;
+import com.example.tomatomall.vo.AccountVO;
+import com.example.tomatomall.vo.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+
+
+@RestController
+@RequestMapping("/api/accounts")
+public class AccountController {
+
+    @Resource
+    private AccountService accountService;
+
+    @Autowired
+    TokenUtil tokenUtil;
+
+    /**
+     * 获取用户详情（需要 token）
+     */
+    @GetMapping("/{username}")
+    public Response<AccountVO> getAccountInfo(@PathVariable String username) {
+        return Response.buildSuccess(accountService.getAccountInfo());
+    }
+
+
+    /**
+     * 创建用户（注册）
+     */
+    @PostMapping("/create")
+    public Response<String> createAccount(@RequestBody AccountVO accountVO) {
+        return Response.buildSuccess(accountService.createAccount(accountVO));
+    }
+
+    /**
+     * 更新用户信息（需要 token）
+     */
+    @PostMapping("/update")
+    public Response<String> updateAccount(@RequestBody AccountVO accountVO) {
+        return Response.buildSuccess(accountService.updateAccount(accountVO));
+    }
+
+    /**
+     * 用户登录
+     */
+    @PostMapping("/login")
+    public Response<String> login(@RequestBody AccountVO accountVO) {
+        return Response.buildSuccess(accountService.login(accountVO));
+    }
+
+    @PostMapping("/image")
+    public Response<String> upload(@RequestParam MultipartFile file){
+        return Response.buildSuccess(accountService.uploadImg(file));
+    }
+
+//    private Response<Boolean> checkToken(String Token){
+//
+//    }
+
+
+}
---
+++ TomatoMall/src/main/java/com/example/tomatomall/exception/TomatoMallException.java
@@ -0,0 +1,60 @@
+package com.example.tomatomall.exception;
+
+/**
+ * @Author: DingXiaoyu
+ * @Date: 0:26 2023/11/26
+ * 你可以在这里自定义Exception
+ */
+public class TomatoMallException extends RuntimeException {
+
+    private String code;
+
+    // 构造方法，接受错误信息和错误码
+    public TomatoMallException(String message, String code) {
+        super(message);
+        this.code = code;
+    }
+
+    // 仅接受错误信息的构造方法，默认错误码为"500"
+    public TomatoMallException(String message) {
+        super(message);
+        this.code = "500";  // 默认的错误码，代表内部服务器错误
+    }
+
+    // 获取错误码
+    public String getCode() {
+        return code;
+    }
+
+    // 定义一些常见的异常
+    public static TomatoMallException usernameAlreadyExists() {
+        return new TomatoMallException("用户名已经存在!", "400");
+    }
+
+    public static TomatoMallException notLogin() {
+        return new TomatoMallException("未登录!", "401");
+    }
+
+    public static TomatoMallException usernameOrPasswordError() {
+        return new TomatoMallException("用户名或密码错误!", "400");
+    }
+
+    public static TomatoMallException usernameNotExists() {
+        return new TomatoMallException("用户名不存在", "404");
+    }
+
+    // 提供一个静态方法用于抛出文件上传失败的异常
+    public static TomatoMallException fileUploadFail() {
+        return new TomatoMallException("文件上传失败，请检查文件格式或网络连接！", "500");
+    }
+
+    // 你可以根据不同的异常类型，返回不同的错误码
+    public static TomatoMallException badRequest(String message) {
+        return new TomatoMallException(message, "400");
+    }
+
+    public static TomatoMallException forbidden() {
+        return new TomatoMallException("没有权限", "403");
+    }
+
+}
